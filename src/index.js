00180ee282f991191b7b7550ed5067454577c4dd
Vola Graholska <31925515+Vola-G@users.noreply.github.com>
2017-10-01 15:57:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ module.exports = function multiply(first, second) {
 var num1 = parseInt(first,10);
 var num2 = parseInt(second,10);
 var result = num1*num2;
-console.log(result);
+  return result;
 }
